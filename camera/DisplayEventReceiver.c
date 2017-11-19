f6044f7af9285f8ceefcaa2aec8c280fb9d1d069
Pranav Vashi <neobuddy89@gmail.com>
2017-11-19 10:42:39 +0530

    cheeseburger: Shim camera
    Change-Id: I52ab762e895424f4e447da44cb8c7e9f29c6eb73
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- camera/DisplayEventReceiver.c
+++ camera/DisplayEventReceiver.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2017 The LineageOS Project
+ * Copyright (C) 2016 The CyanogenMod Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,4 +14,6 @@
  * limitations under the License.
  */
 
-const char *_ZN7android18gClientPackageNameE;
+/* TODO: Actually provide implementations for these function! */
+
+void _ZN7android20DisplayEventReceiverC1Ev() {}
